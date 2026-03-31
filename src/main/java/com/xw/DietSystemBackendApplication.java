9b9c2e8053794b91245e6d7f35caec59dd6a745f
XW <w3163029937@163.com>
2026-03-31 22:26:49 +0800

    项目的创建

---
+++ src/main/java/com/xw/DietSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.xw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DietSystemBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xw/DietSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
